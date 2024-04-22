f37517d82a9b7618f40f6384acebba092bd8f5cd
Robbozinoz <30688053+Robbozinoz@users.noreply.github.com>
2024-04-22 11:55:19 +0800

    scoping exercise

---
+++ session-09/assets/js/challenge_2.js
@@ -0,0 +1,3 @@
+// Event handler function for click event
+
+// Assigning event handler to a button element
---
+++ session-11/assets/js_starter/scope.js
@@ -0,0 +1,33 @@
+// Step 1 Global
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(`global: ${x}`);
+console.log(`global: ${y}`);
+console.log(`global: ${x}`);
+
+function localFuntion() {
+  // Step 2 Local
+  var x = 10;
+  const z = 5;
+
+  console.log(`local: ${x}`);
+  console.log(`local: ${y}`);
+  console.log(`local: ${z}`);
+
+  {
+    // Step 3 Block
+    var x = 11;
+    const z = 6;
+
+    console.log(`block: ${x}`);
+    console.log(`block: ${y}`);
+    console.log(`block: ${z}`);
+  }
+
+  //   Step 4 Function scoping
+  //   console.log(`local: ${x}`);
+  //   console.log(`local: ${y}`);
+  //   console.log(`local: ${z}`);
+}
