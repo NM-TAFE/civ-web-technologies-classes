0f30285282dfde5bda1261b0ef40f813aaac8ec8
John Robertson <ROBERJ@tdm.local>
2024-07-15 08:30:39 +0800

    feat:update session 17

--- session-17/assets/js/scripts_challenge_2.js
+++ session-17/assets/js/scripts_challenge_2.js
@@ -7,14 +7,14 @@ let data = {
 // The template
 let template = function (props) {
 	let html =
-			'<ul>' +
-                // Loop through the props array wrapping each item in a <li>
-				props.books.map(function (book) {
-					return '<li>' + book + '</li>';
-					}).join('') +
-				'</ul>';
-			return html;
-		};
+		'<ul>' +
+		// Loop through the props array wrapping each item in a <li>
+		props.books.map(function (book) {
+			return '<li>' + book + '</li>';
+		}).join('') +
+		'</ul>';
+	return html;
+};
 
 // Render the template into the UI
 let app = document.querySelector('#app');
@@ -25,5 +25,4 @@ data.books.push('Harry Potter IV');
 data.books.push('Harry Potter V');
 app.innerHTML = template(data);
 
-  
-  
+
