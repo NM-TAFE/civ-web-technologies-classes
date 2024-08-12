dfb3d023857a6ec802311f89247e1a8e7d1e9c54
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2024-08-12 15:01:12 +0800

    feat: session 8 update

---
+++ session-08/assets/js/challenge4_code/sum.js
@@ -0,0 +1,10 @@
+function calculateSum(start, finish) {
+    let sum = start + finish;
+    return sum;
+}
+
+const number1 = 5;
+const number2 = 7;
+
+const result = calculateSum(number1, number2);
+console.log(`The sum of ${number1} and ${number2} is ${result}`);
