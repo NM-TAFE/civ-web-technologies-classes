ec4dbfb46786908e6e5c97d77fa97ee10cd7ac67
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2024-09-09 15:10:03 +0800

    feat: isNan file to run in concole

---
+++ session-09/assets/downloads/isNaN.md
@@ -0,0 +1,28 @@
+```
+let divide = 'Hi'/0
+undefined
+divide
+NaN
+isNaN(divide)
+true
+divide == 'undefined'
+false
+divide == undefined
+false
+divide == null
+false
+multiply
+multiply == undefined
+var multiply
+undefined
+multiply == undefined
+true
+multiply == null
+true
+multiply === null
+false
+multiply === undefined
+true
+isNaN(multiply)
+true
+```
---
+++ session-12/assets/js_starter/function_scope.js
@@ -0,0 +1,35 @@
+// Step 1: Global context
+this.globalName = "Global Context";
+
+console.log(`global: ${this.globalName}`); // Output: Global Context
+
+const anObject = {
+  name: "Object Context",
+
+  traditionalFunction: function () {
+    // Step 2: Traditional function context
+    console.log(`traditional function: ${this.name}`); // Output: Object Context
+
+    function nestedFunction() {
+      // Step 3: Nested traditional function context
+      console.log(`nested traditional function: ${this.name}`); // Output: undefined
+    }
+
+    nestedFunction();
+  },
+
+  arrowFunction: function () {
+    // Step 4: Arrow function context
+    console.log(`arrow function: ${this.name}`); // Output: Object Context
+
+    const nestedArrowFunction = () => {
+      // Step 5: Nested arrow function context
+      console.log(`nested arrow function: ${this.name}`); // Output: Object Context
+    };
+
+    nestedArrowFunction();
+  },
+};
+
+anObject.traditionalFunction();
+anObject.arrowFunction();
