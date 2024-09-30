bb7ae800029362968a2aaabc7a9381b89c984098
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2024-10-01 07:54:57 +0800

    chore: update session 11

--- session-11/assets/js_starter/scripts_challenge_5.js
+++ session-11/assets/js_starter/scripts_challenge_5.js
@@ -6,9 +6,6 @@ function toggleSidebarContent() {
 }
 
 // Add a button to toggle the sidebar content
-const toggleButton = document.createElement('button');
-toggleButton.textContent = 'Toggle Sidebar Content';
-toggleButton.addEventListener('click', toggleSidebarContent);
+
 
 // Append the button to the body
-document.body.appendChild(toggleButton);
