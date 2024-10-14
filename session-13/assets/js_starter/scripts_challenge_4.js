c46119dfa788ab6e4b5056bfba5a3f5944aa2722
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2024-10-15 07:11:19 +0800

    feat: update session

--- session-13/assets/js_starter/scripts_challenge_4.js
+++ session-13/assets/js_starter/scripts_challenge_4.js
@@ -1,20 +1,15 @@
 // Challenge 4 Starter
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then(function (response) {
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(function (response) {
     // If the response is successful, get the JSON
-    if (response.ok) {
-        return response.json();
-    }
-
     // Otherwise, throw an error
-    throw response.status;
-})
-.then(function (data) {
+  })
+  .then(function (data) {
     // This is the JSON from our response
     // Create and display the list of posts here
     // Modify this part of the code
-})
-.catch(function (error) {
+  })
+  .catch(function (error) {
     // There was an error
     console.warn(error);
-});
+  });
