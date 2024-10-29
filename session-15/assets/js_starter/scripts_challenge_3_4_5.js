592ac3f5bdde1216d7a833e613be3539ba3a6b32
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2024-10-29 10:08:50 +0800

    feat: session 15 challenge 3-5

--- session-15/assets/js_starter/scripts_challenge_3_4_5.js
+++ session-15/assets/js_starter/scripts_challenge_3_4_5.js
@@ -1,20 +1,16 @@
 // Challenge 3
-function originalFunction() {
-    // Start fetching data from the server
-    fetch('https://jsonplaceholder.typicode.com/posts/') // Step 1: Initiate an HTTP request
-        .then(function (response) {
-            // Step 2: Wait for the response to arrive
-            return response.json(); // Step 3: Parse the response as JSON
-        })
-        .then(function (data) {
-            // Step 4: Handle the parsed JSON data
-            console.log('Traditional Fetch', data); // Step 5: Display the data
-        });
+/**
+ * Get an article by id
+ * @param id
+ */
+async function getArticleByID(id) {
+  let response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
 
-    // This message will be displayed immediately
-    console.log('Traditional Message'); // Step 6: Display this message
+  let data = await response.json();
+
+  console.log(data);
 }
 
-originalFunction();
-
-
+getArticleByID(1);
