697c2a720d812e21b0ed3eca5ba10ee3dbe9081e
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2024-10-29 09:30:16 +0800

    feat: update session 15 challenge1

--- session-15/assets/js_starter/scripts_challenge_1.js
+++ session-15/assets/js_starter/scripts_challenge_1.js
@@ -1,39 +1,39 @@
 // Challenge 1
 // Import the array of books
-import books from "/books.js"
+import books from "./books.js";
 
 // Find a book by ISBN
 const findBookByISBN = (ISBN) => {
-    // Use the find function to search for a ISBN number (checking against book.ISBN)
-
-    // If found log a title
-
+  // Use the find function to search for a ISBN number (checking against book.ISBN)
+  const foundBook = books.find((book) => book.ISBN === ISBN);
+  // If found log a title
+  if (foundBook) {
+    console.log(`Found book: ${foundBook.title}`);
+  } else {
+    console.log("Not found");
+  }
 };
 
 // Double the prices of all books
 const doublePrices = () => {
-    // Create a constant which takes the result of mapping over the books
+  // Create a constant which takes the result of mapping over the books
+  // Use each iteration to double the price of the book
+  const doubledPrices = books.map((book) => ({ price: book.price * 2 }));
 
-        // Use each iteration to double the price of the book 
-        // adding it to doubledPrices
-
-
-    console.log("Doubled prices:", doubledPrices);
+  // adding it to doubledPrices
+  console.log("Doubled prices:", doubledPrices);
 };
 
 // Calculate the total price of all books
 const calculateTotalPrice = () => {
-    // Create a constant that take the result of the reduce function
+  // Create a constant that take the result of the reduce function
+  const totalPrice = books.reduce((total, book) => total + book.price, 0);
+  // Also add a total accumulator to the reduce function arguments
 
-    // Also add a total accumulator to the reduce function arguments
-
-    console.log("Total price of all books:", totalPrice);
+  console.log("Total price of all books:", totalPrice);
 };
 
 // DONT FORGET to Call the functions
 findBookByISBN("ISBN-222");
 doublePrices();
 calculateTotalPrice();
-
-
-
