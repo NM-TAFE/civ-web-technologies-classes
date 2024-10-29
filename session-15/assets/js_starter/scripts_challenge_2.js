558c7ffae38bae10500c48c2871b16be2369c3e9
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2024-10-29 08:10:38 +0800

    feat: session 15

---
+++ session-15/assets/js_starter/scripts_challenge_2.js
@@ -0,0 +1,18 @@
+// Challenge 2
+// Import the array of books
+import books from "/books.js"
+
+// Calculate the total price of all books using a callback
+// Pass both the books array and the callback function
+function calculateTotalPrice(books, callback) {
+
+}
+
+// Callback function to display the total price
+function displayTotalPrice() {
+
+}
+
+// Call the main function passing in the books and the callback
+calculateTotalPrice();
+
